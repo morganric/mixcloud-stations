6021635ea91f50ca7ff8889ac90829c45d6a1482
Richard Morgan <morganric@gmail.com>
2013-08-09 13:18:46 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MixcloudStations::Application.config.secret_token = '1f29dc7b2fd13253b1fc7b7c822cea0ae7ff8cf79a3b9c2e7b6b8e0dce754125c546d25167c1d44fd3414406ac0cc92325a3bde00d8484cf58ba359828fbe921'
